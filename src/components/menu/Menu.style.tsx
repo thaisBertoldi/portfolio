3fa9768de06b792886de76d50a891c020cc467fb
Thais B <thais_brt@hotmail.com>
2022-05-09 11:39:31 -0300

    menu hamburguer inserido no header

--- src/components/menu/Menu.style.tsx
+++ src/components/menu/Menu.style.tsx
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 
 export const ContainerMenu = styled.div`
   display: flex;
+  @media (max-width: 768px) {
+    display: none;
+  }
 `;
 
 export const List = styled.ul`
@@ -21,9 +24,37 @@ export const Item = styled.li`
   -moz-transition: all 0.3s;
   -webkit-transition: all 0.3s;
   transition: all 0.3s;
+  list-style: none;
   :hover {
     -moz-transform: scale(1.1);
     -webkit-transform: scale(1.1);
     transform: scale(1.1);
+  }
+`;
+
+
+export const MenuHamburguer = styled.div`
+  color: white;
+  font-size: 20px;
+  display: none;
+  @media (max-width: 768px) {
+    display: flex;
+    padding-left: 75%;
+  }
+`;
+
+export const HamburguerDiv = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 8%;
+  a {
+    color: #dde2ff;
+    text-decoration: none; 
+    padding-left: 70%;
+  }
+  @media (min-width: 768px) {
+    display: none;
   }
 `;
--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,21 +1,49 @@
-import { ContainerMenu, Item, LinkCustom, List } from "./Menu.style";
+import { useState } from "react";
+import { MdMenu } from "react-icons/md";
+import {
+  ContainerMenu,
+  HamburguerDiv,
+  Item,
+  LinkCustom,
+  List,
+  MenuHamburguer,
+} from "./Menu.style";
 
 export default function Menu() {
+  const [openHamburguer, setOpenHamburguer] = useState<boolean>(false);
   return (
-    <ContainerMenu>
-      <nav>
-        <List>
-          <Item>
-            <LinkCustom to="/">Sobre mim</LinkCustom>
-          </Item>
-          <Item>
-            <LinkCustom to="/repositories">Repositórios</LinkCustom>
-          </Item>
-          <Item>
-            <LinkCustom to="/contact">Contato</LinkCustom>
-          </Item>
-        </List>
-      </nav>
-    </ContainerMenu>
+    <>
+      <MenuHamburguer>
+        <MdMenu onClick={() => setOpenHamburguer(!openHamburguer)} />
+      </MenuHamburguer>
+      {openHamburguer && (
+        <HamburguerDiv>
+            <Item>
+              <LinkCustom to="/">Sobre mim</LinkCustom>
+            </Item>
+            <Item>
+              <LinkCustom to="/repositories">Repositórios</LinkCustom>
+            </Item>
+            <Item>
+              <LinkCustom to="/contact">Contato</LinkCustom>
+            </Item>
+        </HamburguerDiv>
+      )}
+      <ContainerMenu>
+        <nav>
+          <List>
+            <Item>
+              <LinkCustom to="/">Sobre mim</LinkCustom>
+            </Item>
+            <Item>
+              <LinkCustom to="/repositories">Repositórios</LinkCustom>
+            </Item>
+            <Item>
+              <LinkCustom to="/contact">Contato</LinkCustom>
+            </Item>
+          </List>
+        </nav>
+      </ContainerMenu>
+    </>
   );
 }
